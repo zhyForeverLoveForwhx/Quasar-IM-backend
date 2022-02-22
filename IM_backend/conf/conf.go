01c1202f151c77a708123309c0948a91b9111178
zhy <1654049765@qq.com>
2022-02-22 14:50:22 +0800

    conf初始化

---
+++ IM_backend/conf/conf.go
@@ -0,0 +1,25 @@
+package conf
+
+//初始化
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+
+	RedisDb     string
+	RedisAddr   string
+	RedisPw     string
+	RedisDbName string
+
+	MongoDBName string
+	MongoDBAddr string
+	MongoDBuser string
+	MongoDBPwd  string
+	MongoDBPort string
+)
