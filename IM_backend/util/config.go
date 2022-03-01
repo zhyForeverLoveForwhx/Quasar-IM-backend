078edff73f553bc2cf955baf1a2f8aa1a8b5c9ed
Awadabang <1765256254@qq.com>
2022-03-01 11:27:14 +0800

    env

--- IM_backend/util/config.go
+++ IM_backend/util/config.go
@@ -9,12 +9,15 @@ import (
 //Config stores all configuration of the application
 //The values are read by viper from a config file or environment varibles
 type Config struct {
-	DBDriver      		string		   `mapstructure:"DB_DRIVER"`
-	DBSource      		string		   `mapstructure:"DB_SOURCE"`
-	DBTESTSOURCE	    string		   `mapstructure:"DB_TEST_SOURCE"`
-	ServerAddress 		string		   `mapstructure:"SERVER_ADDRESS"`
-	TokenSymmetricKey   string  	   `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	AccessTokenDuration time.Duration  `mapstructure:"ACCESS_TOKEN_DURATION"`
+	AppMode             string        `mapstructure:"APP_MODE"`
+	HttpPort            string        `mapstructure:"HTTP_PORT"`
+	RedisDBSource       string        `mapstructure:"REDIS_DB_SOURCE"`
+	MongoDBSource       string        `mapstructure:"MONGO_DB_SOURCE"`
+	MysqlDBSource       string        `mapstructure:"MYSQL_DB_SOURCE"`
+	DBTESTSOURCE        string        `mapstructure:"DB_TEST_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 //LoadConfig reads configuration from file or environment variables
@@ -22,7 +25,7 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
-	
+
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
